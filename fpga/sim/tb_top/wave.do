8851a10fe93bd8003033ab3f3e8a3fe4f45ed00f
安 卢 <laa6203@163.com>
2017-07-07 17:52:04 +0800

    src/ov_inf/iic_inf.v

--- fpga/sim/tb_top/wave.do
+++ fpga/sim/tb_top/wave.do
@@ -9,16 +9,19 @@ add wave -noupdate /tb/sg1_top/u_commu_top/u_phy_utx/tx_vld
 add wave -noupdate /tb/sg1_top/u_commu_top/u_phy_utx/cnt_us
 add wave -noupdate /tb/sg1_top/u_ov_inf/ov_sioc
 add wave -noupdate /tb/sg1_top/u_ov_inf/ov_siod
+add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/scl_nostop
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/scl_up
-add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/scl_donw
+add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/scl_down
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_reg/now_wr
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_reg/now_rd
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/cfg_iic_devid
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/cfg_iic_addr
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/cfg_iic_wdata
 add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/act_iic_write
+add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/st_iic
+add wave -noupdate /tb/sg1_top/u_ov_inf/u_iic_inf/sda_org
 TreeUpdate [SetDefaultTree]
-WaveRestoreCursors {{Cursor 1} {25627 ns} 0}
+WaveRestoreCursors {{Cursor 1} {21378 ns} 0}
 quietly wave cursor active 1
 configure wave -namecolwidth 317
 configure wave -valuecolwidth 100
@@ -34,4 +37,4 @@ configure wave -griddelta 40
 configure wave -timeline 0
 configure wave -timelineunits ns
 update
-WaveRestoreZoom {0 ns} {31500 ns}
+WaveRestoreZoom {20874 ns} {21866 ns}
--- fpga/src/ov_inf/iic_inf.v
+++ fpga/src/ov_inf/iic_inf.v
@@ -51,8 +51,8 @@ always  @(posedge clk_sys or negedge rst_n)	begin
 	else ;
 end
 
-wire scl_up = (cnt_us == 4'h3) & pluse_us;
-wire scl_donw = (cnt_us == 4'h8) & pluse_us;
+wire scl_up = (cnt_us == 4'h2) & pluse_us;
+wire scl_down = (cnt_us == 4'h7) & pluse_us;
 
 
 reg scl_nostop;
@@ -68,24 +68,27 @@ end
 
 //--------- main FSM ------------
 parameter S_IDLE = 5'h0;
-parameter S_WSTART = 5'h1;
-parameter S_WDAT1 = 5'h2;
-parameter S_WACK1 = 5'h3;
-parameter S_WDAT2 = 5'h4;
-parameter S_WACK2 = 5'h5;
-parameter S_WDAT3 = 5'h6;
-parameter S_WACK3 = 5'h7;
+parameter S_WSTART1 = 5'h1;
+parameter S_WSTART2 = 5'h2;
+parameter S_WDAT1 = 5'h3;
+parameter S_WACK1 = 5'h4;
+parameter S_WDAT2 = 5'h5;
+parameter S_WACK2 = 5'h6;
+parameter S_WDAT3 = 5'h7;
+parameter S_WACK3 = 5'h8;
 parameter S_STOP = 5'he;
 parameter S_DONE = 5'hf;
-parameter S_RSTART1 = 5'h11;
-parameter S_RDAT1 = 5'h12;
-parameter S_RACK1 = 5'h13;
-parameter S_RDAT2 = 5'h14;
-parameter S_RACK2 = 5'h15;
-parameter S_RSTART2 = 5'h10;
-parameter S_RDAT3 = 5'h16;
-parameter S_RACK3 = 5'h17;
-parameter S_RDAT4 = 5'h18;
+parameter S_RSTART11 = 5'h11;
+parameter S_RSTART12 = 5'h12;
+parameter S_RDAT1 = 5'h13;
+parameter S_RACK1 = 5'h14;
+parameter S_RDAT2 = 5'h15;
+parameter S_RACK2 = 5'h16;
+parameter S_RSTART21 = 5'ha;
+parameter S_RSTART22 = 5'hb;
+parameter S_RDAT3 = 5'h17;
+parameter S_RACK3 = 5'h18;
+parameter S_RDAT4 = 5'h19;
 parameter S_RACK4 = 5'h1a;
 
 reg [4:0] st_iic;
@@ -96,8 +99,10 @@ always @ (posedge clk_sys or negedge rst_n)	begin
 		st_iic <= S_IDLE;
 	else begin
 		case(st_iic)
-			S_IDLE : st_iic <=	act_write ? S_WSTART :
-													act_read ? S_RSTART1 : S_IDLE;
+			S_IDLE : st_iic <=	act_write ? S_WSTART1 :
+													act_read ? S_RSTART11 : S_IDLE;
+			S_WSTART1 : st_iic <= scl_up ? S_WSTART2 :  S_WSTART1;
+			S_WSTART2 : st_iic <= scl_down ? S_WDAT1 : S_WSTART2;
 			default :  st_iic <= S_IDLE;
 		endcase
 	end
@@ -110,8 +115,38 @@ assign act_read  = act_iic_read[1]  & act_iic_read[0];
 
 
 
+
+//----------- data output -------------
+reg [7:0] tmp_devid;
+reg [7:0] tmp_addr;
+reg [7:0] tmp_data;
+always @ (posedge clk_sys or negedge rst_n)	begin
+	if(~rst_n)	begin
+		tmp_devid <= 8'h0;
+		tmp_addr <= 8'h0;
+		tmp_data <= 8'h0;
+	end
+	else if((st_iic == S_WSTART1)||(st_iic == S_RSTART11))	begin
+		tmp_devid <= cfg_iic_devid;
+		tmp_addr <= cfg_iic_addr;
+		tmp_data <= cfg_iic_wdata;
+	end
+	else if((st_iic == S_WDAT1)||(st_iic == S_RDAT1)||(st_iic == S_RDAT3))
+		tmp_devid <= scl_down ? {tmp_devid[6:0],1'b0} : tmp_devid;
+	else if((st_iic == S_WDAT2)||(st_iic == S_RDAT2))
+		tmp_addr <= scl_down ? {tmp_addr[6:0],1'b0} : tmp_addr;
+	else if(st_iic == S_WDAT3)
+		tmp_data <= scl_down ? {tmp_data[6:0],1'b0} : tmp_data;
+end
+
+
+		
 wire scl = 1'bz;//scl_nostop;
-wire sda = 1'bz;
+wire sda_org;
+assign sda_org = 	(st_iic == S_WSTART1) ? 1'b1 :
+									(st_iic == S_WSTART2) ? 1'b0 :	
+									(st_iic == S_WDAT1)|(st_iic == S_RDAT1) ? tmp_devid[7] :
+									(st_iic == S_WDAT2)|(st_iic == S_RDAT2) ? tmp_devid[7] :1'bz;
 
 endmodule
 
