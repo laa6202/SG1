8e3c6a1afa7789949f5d0f7f96be0cb0a4a5debf
安 卢 <laa6203@163.com>
2017-10-09 17:09:27 +0800

    add smg

---
+++ fpga_vfd/src/hmi_top/freq_show.v
@@ -0,0 +1,13 @@
+//freq_show.v
+
+module freq_show(
+freq,
+
+);
+input [9:0]	freq;
+
+//-----------------------------------
+
+wire [3:0] 
+
+endmodule
--- fpga_vfd/src/hmi_top/hmi_top.v
+++ fpga_vfd/src/hmi_top/hmi_top.v
@@ -5,20 +5,28 @@ module hmi_top(
 sw,
 key,
 freq,
+nixie0,
+nixie1,
+nixie2,
 //clk rst
 clk_sys,
 rst_n
 );
 input sw;
-input [7:0]		key;
-output [7:0]	freq;
+input [2:0]		key;
+output [9:0]	freq;
+output [7:0]	nixie0;
+output [7:0]	nixie1;
+output [7:0]	nixie2;
 //clk rst
 input clk_sys;
 input rst_n;
 //-----------------------------------
 //-----------------------------------
 
-wire [7:0] freq = key;
+reg [9:0] freq_org = 10'd50;
+
+wire [7:0] freq;
 
 endmodule
 
